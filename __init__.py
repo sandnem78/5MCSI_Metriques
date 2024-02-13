8c1eabb3b6b0219e9ce4f3bf27e876fb5adcb328
sandnem78 <159912769+sandnem78@users.noreply.github.com>
2024-02-13 14:18:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('Sandosh Namassivayane') #committ
+    return render_template('hello.html') #committ
   
 if __name__ == "__main__":
   app.run(debug=True)
